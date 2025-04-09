9d1be8ca56100961dae2fc68e0b392e7c27561d1
Sebastian Wojdanowski <sebastian.wojdanowski@flip.shop>
2025-04-09 17:24:35 +0200

    feat(fetch binance data in batches): add async generator to fetch binance data in batches

--- src/binance-stats/binance-stats.service.ts
+++ src/binance-stats/binance-stats.service.ts
@@ -3,7 +3,10 @@ import {
   GetBinanceStatsParams,
   GetBinanceStatsResponse,
 } from './dto/get-binance-stats.dto';
-import { BinanceClientService } from 'src/binance-client/binance-client.service';
+import {
+  BinanceAggTradesResponse,
+  BinanceClientService,
+} from 'src/binance-client/binance-client.service';
 import { start } from 'repl';
 
 @Injectable()
@@ -15,8 +18,40 @@ export class BinanceStatsService {
   public async getBinanceStats(params: GetBinanceStatsParams): Promise<any> {
     // because Binance api does not let you provide startTime/endTime along with fromId
     const result = await this.getIdRangeBasedOnTimeRange(params);
+    // get batches
+
+    // fetch data
+
+    // group statistics
 
     return;
+  }
+
+  public async *getTradesBatches(
+    startId: number,
+    endId: number,
+    symbol: string,
+  ): AsyncGenerator<BinanceAggTradesResponse[], void, undefined> {
+    let fromId = startId;
+
+    while (true) {
+      if (fromId > endId) {
+        break;
+      }
+
+      const data = await this.binanceClientService.fetchTrades({
+        fromId,
+        symbol,
+      });
+
+      if (!data.length) {
+        break;
+      }
+
+      fromId = data.at(-1)!.a + 1;
+
+      yield data;
+    }
   }
 
   private async getIdRangeBasedOnTimeRange(
