773e8edaa5a44e9a97fbf94700d024935883973f
Sebastian Wojdanowski <sebastian.wojdanowski@flip.shop>
2025-04-09 17:15:54 +0200

    feat(id range): add method to get id range based on provided time

--- src/binance-stats/binance-stats.module.ts
+++ src/binance-stats/binance-stats.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BinanceStatsService } from './binance-stats.service';
 import { BinanceStatsController } from './binance-stats.controller';
+import { BinanceClientModule } from 'src/binance-client/binance-client.module';
+import { BinanceClientService } from 'src/binance-client/binance-client.service';
 
 @Module({
-  imports: [],
+  imports: [BinanceClientModule],
   controllers: [BinanceStatsController],
-  providers: [BinanceStatsService],
+  providers: [BinanceStatsService, BinanceClientService],
 })
 export class BinanceStatsModule {}
--- src/binance-stats/binance-stats.service.ts
+++ src/binance-stats/binance-stats.service.ts
@@ -3,14 +3,43 @@ import {
   GetBinanceStatsParams,
   GetBinanceStatsResponse,
 } from './dto/get-binance-stats.dto';
+import { BinanceClientService } from 'src/binance-client/binance-client.service';
+import { start } from 'repl';
 
 @Injectable()
 export class BinanceStatsService {
   private readonly logger = new Logger(this.constructor.name);
 
-  constructor() {}
+  constructor(private readonly binanceClientService: BinanceClientService) {}
 
   public async getBinanceStats(params: GetBinanceStatsParams): Promise<any> {
+    // because Binance api does not let you provide startTime/endTime along with fromId
+    const result = await this.getIdRangeBasedOnTimeRange(params);
+
     return;
+  }
+
+  private async getIdRangeBasedOnTimeRange(
+    params: GetBinanceStatsParams,
+  ): Promise<{
+    startId: number;
+    endId: number;
+  }> {
+    const [startRecord, endRecord] = await Promise.all([
+      this.binanceClientService.fetchTrades({
+        symbol: params.symbol,
+        startTime: params.startTime,
+        limit: 1,
+      }),
+      this.binanceClientService.fetchTrades({
+        symbol: params.symbol,
+        startTime: params.endTime,
+        limit: 1,
+      }),
+    ]);
+    return {
+      startId: startRecord[0].a,
+      endId: endRecord[0].a,
+    };
   }
 }
